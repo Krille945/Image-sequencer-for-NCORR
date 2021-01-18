eadda7f72f075f38f49bacd305addfdbefe96a9f
Christopher Endelt <77574340+Krille945@users.noreply.github.com>
2021-01-18 16:24:11 +0100

    Add files via upload

--- Video_with_ncorr.m
+++ Video_with_ncorr.m
@@ -1,7 +1,7 @@
-clc;    % ryd command window
-close all;  % luk all figure
-imtool close all;  % luk alle imtool figure.
-clear;  %slet eksisterende variable.
+clc;    % clear command window
+close all;  % close all figures
+imtool close all;  % luk all imtool figures.
+clear;  %slet existing variabels.
 
 scriptpath = matlab.desktop.editor.getActiveFilename;
 scriptpath = erase(scriptpath, '\Video_with_ncorr.m');
@@ -19,40 +19,26 @@ tt = 1;
 while tt < am_vids
 handles_ncorr = ncorr;
 
-%load data til program
+%load data to program
 
-%load reference billede
+%load reference picture
 path = string(g{1,1}(1,tt))+'/Frame_0000.png';
 path_new = strrep (path, '\', '/');
 ref = imread(path_new);
+ 
 
-%for at loade current images skal de stilles op i et cell array til dette
-%laves der et loop 
 
-%sæt hent path op
 dircur = string(g{1,1}(1,tt))+'/*.png';
-%bestemmer længeden af path
 
 
-
-    ca = 'C:\Users\chris\Desktop\Program_efter_aflevering\Data_01\Refbillede 29-10-2020\Frame_0000.png';
-    
-
-
-%diffinere vores current til at være det cell array
-
-
-%deffinere vores region of interest
-%dette kræver at man selv har lavet et billede eller skal det tegnes på
-
+%load region of interest
 %roi = imread('auto hent path før');
 
-%deffinere dataen i ncorr
+%deffine data in ncorr
 handles_ncorr.set_ref(ref);
 handles_ncorr.set_cur(ca);
-%handles_ncorr.set_roi_cur(roi);
+handles_ncorr.set_roi_cur(roi);
 
-%%your code here
 
 
 end
